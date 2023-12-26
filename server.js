d0cea0026c0ad7c291e38da7e1841a8fa04363e8
phantom <phantom@okvip.com>
2023-12-26 22:01:02 +0700

    update

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const path = require("path");
 const morgan = require("morgan");
 var cors = require("cors");
 const app = express();
+const fs = require("fs");
+// Sử dụng multer để xử lý file được gửi từ client
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 app.use(morgan("combined"));
 app.use(
   cors({
@@ -39,6 +45,35 @@ app.use("/api/proposal", proposalRoute);
 app.use("/api/document", documentlRoute);
 app.use("/api/roleaction", roleActionRoute);
 app.use("/api/notify", notifyRoute);
+
+app.put("/api/update-pdf", upload.single("pdf"), async (req, res) => {
+  try {
+    // Lấy thông tin từ body của request
+    const { fileLink } = req.body;
+
+    // Kiểm tra xem fileLink và file đã được gửi từ client chưa
+    if (!fileLink || !req.file) {
+      return res
+        .status(400)
+        .json({ message: "Invalid request. Missing fileLink or pdf." });
+    }
+
+    // Lấy dữ liệu PDF từ req.file
+    const pdfBuffer = req.file.buffer;
+
+    // Nối đường dẫn từ thư mục gốc và fileLink
+    const filePath = path.join(__dirname, "public/", fileLink);
+
+    // Ghi dữ liệu mới vào file
+    fs.writeFileSync(filePath, pdfBuffer);
+
+    // Trả về thông báo thành công
+    res.status(200).json({ message: "File updated successfully." });
+  } catch (error) {
+    console.error("Failed to update file:", error);
+    res.status(500).json({ message: "Failed to update file." });
+  }
+});
 
 app.listen(port, (req, res) => {
   console.log("server listening on port " + port);
